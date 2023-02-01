a3dbd3e5481db2ccc0e83d6c6e64004e0efc9356
Piroska Werner <piroska.werner@gmail.com>
2023-02-01 11:46:03 -0600

    feat: Implement search feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
 import React from 'react';
+import { useState, useRef } from 'react';
 
 function App() {
+
+  const [items, setItems] = useState(['one', 'two', 'three', 'four'])
+  // const newItem: string = useRef(itemName)
+  const [filter, setFilter] = useState('')
+
   return (
     <>
     <div>
       <label htmlFor="filter">Search:</label>
-      <input type="text" id="filter" />
+      <input type="text" id="filter" onChange={(event) => setFilter(event.target.value)} />
     </div>
     <div>
       <label htmlFor="newItem">New Item:</label>
       <input type="text" id="newItem" />
-      <button>Add</button>
+      {/* <button onClick={() = ()}>Add</button> */}
     </div>
     <h2>Items:</h2>
+    <ul>
+      {items.filter(item => item.toLowerCase().includes(filter.toLowerCase())).map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </ul>
     </>
 
     );
